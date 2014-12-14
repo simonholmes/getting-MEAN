f1a830643d427817b03b92d31cce232bd36aba7c
Simon Holmes <sholmes@liveperson.com>
2014-12-14 16:06:22 +0000

    End of chapter 3

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrlMain.index);
 
 module.exports = router;
