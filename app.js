50f79e455f4e5633b41776c28cb505393f97032c
Simon Holmes <sholmes@liveperson.com>
2014-12-17 06:57:48 +0000

    Chapter 4, moving data into the controllers

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./app_server/routes/index');
-var users = require('./app_server/routes/users');
+// var users = require('./app_server/routes/users');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
