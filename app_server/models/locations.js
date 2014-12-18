0e9993e233c81d4ebfccc229487b43df62e5bb52
Simon Holmes <sholmes@liveperson.com>
2014-12-18 07:31:38 +0000

    Adding Chapter 5 code

---
+++ app_server/models/locations.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+
+var reviewSchema = new mongoose.Schema({
+    author: String,
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
+    reviewText: String,
+    createdOn: {
+        type: Date,
+        "default": Date.now
+    }
+});
+
+var openingTimeSchema = new mongoose.Schema({
+    days: {
+        type: String,
+        required: true
+    },
+    opening: String,
+    closing: String,
+    closed: {
+        type: Boolean,
+        required: true
+    }
+});
+
+var locationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: String,
+    rating: {
+        type: Number,
+        "default": 0,
+        min: 0,
+        max: 5
+    },
+    facilities: [String],
+    // Always store coordinates longitude, latitude order.
+    coords: {
+        type: [Number],
+        index: '2dsphere'
+    },
+    openingTimes: [openingTimeSchema],
+    reviews: [reviewSchema]
+});
+
+mongoose.model('Location', locationSchema);
