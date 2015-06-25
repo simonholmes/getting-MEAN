0dffb1d07c6be594440cb17d0898b4b59f014b05
Simon Holmes <sholmes@liveperson.com>
2015-06-25 07:18:40 +0100

    Adding login/register api end points

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+
 var ctrlLocations = require('../controllers/locations');
 var ctrlReviews = require('../controllers/reviews');
+var ctrlAuth = require('../controllers/authentication');
 
 router.get('/locations', ctrlLocations.locationsListByDistance);
 router.post('/locations', ctrlLocations.locationsCreate);
@@ -14,5 +16,9 @@ router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
 router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
 router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
 router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+
+// authentication
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
 
 module.exports = router;
